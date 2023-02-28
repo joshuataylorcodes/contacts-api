b084973a03ad1a4c6a3481c651ad2c916f25301d
joshua <joshuamtaylor455@gmail.com>
2023-02-28 15:09:43 -0500

    differentiate routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,22 +1,21 @@
 class ContactsController < ApplicationController
   #to get one contact from database, unhash to test
-  # def a_contact
-  #   contact = Contact.first
-  #   render json: {
-  #     id: contact.id,
-  #     first_name: contact.first_name,
-  #     last_name: contact.last_name,
-  #     email: contact.email,
-  #     phone_number: contact.phone_number,
-  #   }
-  # end
+  def a_contact
+    contact = Contact.first
+    render json: {
+      id: contact.id,
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      email: contact.email,
+      phone_number: contact.phone_number,
+    }
+  end
 
-  #to get all contacts from database, unhash to test
-  # def a_contact
-  #   contacts = Contact.all
-  #   render json: {
-  #     all_contacts: contacts,
-  #   }
-  # end
-
+  # to get all contacts from database, unhash to test
+  def all_contact
+    contacts = Contact.all
+    render json: {
+      all_contacts: contacts,
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/a_contact", controller: "contacts", action: "a_contact"
+
+  get "/all_contact", controller: "contacts", action: "all_contact"
 end
