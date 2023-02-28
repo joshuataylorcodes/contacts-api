2b054ff17adaa3f466e14691d11cf23ab845f48e
joshua <joshuamtaylor455@gmail.com>
2023-02-28 15:03:22 -0500

    storing data in databases + accessing it though my server

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,22 @@
+class ContactsController < ApplicationController
+  #to get one contact from database, unhash to test
+  # def a_contact
+  #   contact = Contact.first
+  #   render json: {
+  #     id: contact.id,
+  #     first_name: contact.first_name,
+  #     last_name: contact.last_name,
+  #     email: contact.email,
+  #     phone_number: contact.phone_number,
+  #   }
+  # end
+
+  #to get all contacts from database, unhash to test
+  # def a_contact
+  #   contacts = Contact.all
+  #   render json: {
+  #     all_contacts: contacts,
+  #   }
+  # end
+
+end
